60f896bdbcbb96efd47191671e48c9429882be63
hwang-chaewon <69956140+hwang-chaewon@users.noreply.github.com>
2022-07-05 13:38:15 +0900

    Add files via upload

---
+++ DAY1/catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/hcw/catkin_ws/src
+# Build directory: /home/hcw/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("dyros_practice_1")
+subdirs("dyros_practice_2")
